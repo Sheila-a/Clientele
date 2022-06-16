8ee5331a0febb6aa4aa8c646ee8d0453ba1410ad
Sheila-a <ugwuodo20@gmail.com>
2022-06-16 19:00:38 +0100

    First Testing

--- src/Home.js
+++ src/Home.js
@@ -1,20 +1,29 @@
-import Imp from "./Components/Importance/Imp";
-import Intro from "./Components/Intro/Intro";
-import Navbar from "./Components/Navbar/Navbar";
-import Resource from "./Components/Resources/Resource";
-import Stuff11 from "./Components/Stuff11/Stuff11";
-import Touch from "./Components/Get In Touch/Touch";
-import Footer from "./Components/Footer/footer";
+import Imp from "./Components/Home/Importance/Imp";
+import Intro from "./Components/Home/Intro/Intro";
+import Navbar from "./Components/Home/Navbar/Navbar";
+import Resource from "./Components/Home/Resources/Resource";
+import Stuff11 from "./Components/Home/Stuff11/Stuff11";
+import Touch from "./Components/Home/Get In Touch/Touch";
+import Footer from "./Components/Home/Footer/footer";
+import OB from "./Components/Home/Our Blog/OB";
+import RWU from "./Components/Home/Reg w Us/RWU";
+import React from "react";
+import "aos/dist/aos.css";
 
 const Home = () => {
   return (
     <div>
+      {/* Approved */}
       <Navbar />
+      {/* Approved */}
       <Intro />
+      {/* Approved */}
       <Stuff11 />
-      <Imp />
-      <Resource />
+      {/* <Imp /> */}
+      {/* <Resource /> */}
+      {/* <OB /> */}
       <Touch />
+      <RWU />
       <Footer />
     </div>
   );
