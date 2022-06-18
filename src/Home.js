212e04429401516d2f6092d18d5ee69bf1389232
Sheila-a <ugwuodo20@gmail.com>
2022-06-18 10:59:02 +0100

    Test for Navbar responsive

--- src/Home.js
+++ src/Home.js
@@ -1,12 +1,12 @@
-import Imp from "./Components/Home/Importance/Imp";
-import Intro from "./Components/Home/Intro/Intro";
+// import Imp from "./Components/Home/Importance/Imp";
+// import Intro from "./Components/Home/Intro/Intro";
 import Navbar from "./Components/Home/Navbar/Navbar";
-import Resource from "./Components/Home/Resources/Resource";
-import Stuff11 from "./Components/Home/Stuff11/Stuff11";
-import Touch from "./Components/Home/Get In Touch/Touch";
-import Footer from "./Components/Home/Footer/footer";
+// import Resource from "./Components/Home/Resources/Resource";
+// import Stuff11 from "./Components/Home/Stuff11/Stuff11";
+// import Touch from "./Components/Home/Get In Touch/Touch";
+// import Footer from "./Components/Home/Footer/footer";
 // import OB from "./Components/Home/Our Blog/OB";
-import RWU from "./Components/Home/Reg w Us/RWU";
+// import RWU from "./Components/Home/Reg w Us/RWU";
 import React from "react";
 import "aos/dist/aos.css";
 
