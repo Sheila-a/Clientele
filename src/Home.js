f41fc18379366469f221bfe3977eddedea2e17de
Sheila-a <ugwuodo20@gmail.com>
2022-06-23 20:07:52 +0100

    Random Test

--- src/Home.js
+++ src/Home.js
@@ -3,7 +3,7 @@ import Intro from "./Components/Home/Intro/Intro";
 import Navbar from "./Components/Home/Navbar/Navbar";
 // import Resource from "./Components/Home/Resources/Resource";
 import Stuff11 from "./Components/Home/Stuff11/Stuff11";
-import Touch from "./Components/Home/Get In Touch/Touch";
+// import Touch from "./Components/Home/Get-In-Touch/Touch";
 // import Footer from "./Components/Home/Footer/footer";
 import OB from "./Components/Home/Our-Blog/OB";
 import RWU from "./Components/Home/Reg w Us/RWU";
@@ -25,7 +25,7 @@ const Home = () => {
       {/* <Resource /> */}
       {/* Approved */}
       <OB />
-      <Touch />
+      {/* <Touch /> */}
       <RWU />
       {/* <Footer /> */}
     </div>
