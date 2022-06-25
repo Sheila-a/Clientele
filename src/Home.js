96b53d5af7af73337a46ef03f6fec5c10c2eb742
Sheila-a <ugwuodo20@gmail.com>
2022-06-25 09:52:02 +0100

    Random Test (6)

--- src/Home.js
+++ src/Home.js
@@ -1,10 +1,10 @@
-// import Imp from "./Components/Home/Importance/Imp";
+import Imp from "./Components/Home/Importance/Imp";
 import Intro from "./Components/Home/Intro/Intro";
 import Navbar from "./Components/Home/Navbar/Navbar";
 import Resource from "./Components/Home/Resources/Resource";
 import Stuff11 from "./Components/Home/Stuff11/Stuff11";
 import Touch from "./Components/Home/Get-In-Touch/Touch";
-// import Footer from "./Components/Home/Footer/footer";
+import Footer from "./Components/Home/Footer/footer";
 import OB from "./Components/Home/Our-Blog/OB";
 import RWU from "./Components/Home/Reg-W-Us/RWU";
 import React from "react";
@@ -20,14 +20,14 @@ const Home = () => {
       {/* Approved */}
       <Stuff11 />
       {/* Not Approved */}
-      {/* <Imp /> */}
+      <Imp />
       {/* Not Approved */}
       <Resource />
       <Touch />
       <RWU />
       {/* Approved */}
       <OB />
-      {/* <Footer /> */}
+      <Footer />
     </div>
   );
 };
