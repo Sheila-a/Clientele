09ea9221610f951d6e48ad59f6e0ee3aed813681
Sheila-a <ugwuodo20@gmail.com>
2022-06-25 11:14:38 +0100

    Test for Footer Responsiveness (9)

--- src/Home.js
+++ src/Home.js
@@ -1,33 +1,33 @@
-import Imp from "./Components/Home/Importance/Imp";
+// import Imp from "./Components/Home/Importance/Imp";
 import Intro from "./Components/Home/Intro/Intro";
 import Navbar from "./Components/Home/Navbar/Navbar";
-import Resource from "./Components/Home/Resources/Resource";
-import Stuff11 from "./Components/Home/Stuff11/Stuff11";
-import Touch from "./Components/Home/Get-In-Touch/Touch";
+// import Resource from "./Components/Home/Resources/Resource";
+// import Stuff11 from "./Components/Home/Stuff11/Stuff11";
+// import Touch from "./Components/Home/Get-In-Touch/Touch";
 import Footer from "./Components/Home/Footer/footer";
-import OB from "./Components/Home/Our-Blog/OB";
-import RWU from "./Components/Home/Reg-W-Us/RWU";
+// import OB from "./Components/Home/Our-Blog/OB";
+// import RWU from "./Components/Home/Reg-W-Us/RWU";
 import React from "react";
 import "aos/dist/aos.css";
 import "./Home.css";
 
 const Home = () => {
   return (
-    <div className="Home">
+    <div>
       {/* Approved */}
       <Navbar />
       {/* Approved */}
       <Intro />
       {/* Approved */}
-      <Stuff11 />
+      {/* <Stuff11 /> */}
       {/* Not Approved */}
-      <Imp />
+      {/* <Imp /> */}
       {/* Not Approved */}
-      <Resource />
-      <Touch />
-      <RWU />
+      {/* <Resource /> */}
+      {/* <Touch /> */}
+      {/* <RWU /> */}
       {/* Approved */}
-      <OB />
+      {/* <OB /> */}
       <Footer />
     </div>
   );
