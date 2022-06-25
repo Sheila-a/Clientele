cc1061c3d9a9f3c7b6a2d869592cc9ec8cf4f671
Sheila-a <ugwuodo20@gmail.com>
2022-06-25 09:48:28 +0100

    Random Test (5)

--- src/Home.js
+++ src/Home.js
@@ -5,7 +5,7 @@ import Resource from "./Components/Home/Resources/Resource";
 import Stuff11 from "./Components/Home/Stuff11/Stuff11";
 import Touch from "./Components/Home/Get-In-Touch/Touch";
 // import Footer from "./Components/Home/Footer/footer";
-// import OB from "./Components/Home/Our-Blog/OB";
+import OB from "./Components/Home/Our-Blog/OB";
 import RWU from "./Components/Home/Reg-W-Us/RWU";
 import React from "react";
 import "aos/dist/aos.css";
@@ -26,7 +26,7 @@ const Home = () => {
       <Touch />
       <RWU />
       {/* Approved */}
-      {/* <OB /> */}
+      <OB />
       {/* <Footer /> */}
     </div>
   );
