4d1515dd816431e35e2366840fa50e701967d87a
Sheila-a <ugwuodo20@gmail.com>
2022-06-25 09:42:42 +0100

    Random Test (4)

--- src/Home.js
+++ src/Home.js
@@ -3,9 +3,9 @@ import Intro from "./Components/Home/Intro/Intro";
 import Navbar from "./Components/Home/Navbar/Navbar";
 import Resource from "./Components/Home/Resources/Resource";
 import Stuff11 from "./Components/Home/Stuff11/Stuff11";
-// import Touch from "./Components/Home/Get-In-Touch/Touch";
+import Touch from "./Components/Home/Get-In-Touch/Touch";
 // import Footer from "./Components/Home/Footer/footer";
-import OB from "./Components/Home/Our-Blog/OB";
+// import OB from "./Components/Home/Our-Blog/OB";
 import RWU from "./Components/Home/Reg-W-Us/RWU";
 import React from "react";
 import "aos/dist/aos.css";
@@ -23,9 +23,9 @@ const Home = () => {
       {/* <Imp /> */}
       {/* Not Approved */}
       <Resource />
-      {/* <Touch /> */}
+      <Touch />
       {/* Approved */}
-      <OB />
+      {/* <OB /> */}
       <RWU />
       {/* <Footer /> */}
     </div>
