df61385fa8793090cf1ba1e760b213271d924437
Sheila-a <ugwuodo20@gmail.com>
2022-06-26 14:33:32 +0100

    Test for Career Responsiveness

---
+++ src/Components/Our-Services/Services/Diagnosis/Diagnosis.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from "../../../Home/Navbar/Navbar";
+
+const Diagnosis = () => {
+  return (
+    <div>
+      <Navbar />
+      Diagnosis
+    </div>
+  );
+};
+
+export default Diagnosis;
