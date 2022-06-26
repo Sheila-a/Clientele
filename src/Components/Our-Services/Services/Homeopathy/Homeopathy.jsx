e010b5dd27272934940f7bb0ab3494c75f16707b
Sheila-a <ugwuodo20@gmail.com>
2022-06-26 16:19:45 +0100

    Test for Homeopathy Responsiveness

--- src/Components/Our-Services/Services/Homeopathy/Homeopathy.jsx
+++ src/Components/Our-Services/Services/Homeopathy/Homeopathy.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Navbar from "../../../Home/Navbar/Navbar";
 import "./Homeopathy.css";
-import Footer from "../../../Home/Footer/footer";
+// import Footer from "../../../Home/Footer/footer";
 
 const Homeopathy = () => {
   return (
@@ -68,10 +68,8 @@ const Homeopathy = () => {
             pariatur omnis eveniet placeat earum.
           </p>
         </div>
-        {/* <br />
-        <br /> */}
       </div>
-      <Footer />
+      {/* <Footer /> */}
     </div>
   );
 };
