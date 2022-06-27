880df6e7fe10683a33e492962819080d2a87ca03
Sheila-a <ugwuodo20@gmail.com>
2022-06-27 12:41:47 +0100

    Updated App.js

--- src/App.js
+++ src/App.js
@@ -2,16 +2,19 @@ import React from "react";
 import Register from "./Components/Register/Register";
 import Home from "./Home";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import About from "./Components/About/About";
+import OurDoctor from "./Components/About-Us/Our-Doctor/About";
+import OurStaff from "./Components/About-Us/Our-Staff/About";
 import Services from "./Components/Our-Services/Services";
 import Contact from "./Components/Contact/Contact";
+import Covid from "./Components/Covid-19/CV_19";
 import Career from "./Components/Our-Services/Services/Career/Career";
 import Consultation from "./Components/Our-Services/Services/Consultation/Consultation";
 import Diagnosis from "./Components/Our-Services/Services/Diagnosis/Diagnosis";
 import FDoc from "./Components/Our-Services/Services/For Doctors/FDoc";
 import Homeopathy from "./Components/Our-Services/Services/Homeopathy/Homeopathy";
-import PTherapy from "./Components/Our-Services/Services/Physical Therapy/PTherapy";
+import PTherapy from "./Components/Our-Services/Services/Physical-Therapy/PTherapy";
 import Appointment from "./Components/Appointment/Appointment";
+import CEvents from "./Components/About-Us/Community-Events/C-Events";
 
 const App = () => {
   return (
@@ -19,8 +22,11 @@ const App = () => {
       <div>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
+          <Route path="/covid-19-policy" element={<Covid />} />
+          <Route path="/meet-our-doctor" element={<OurDoctor />} />
+          <Route path="/community-events" element={<CEvents />} />
           <Route path="/our-services" element={<Services />} />
+          <Route path="/meet-our-staff" element={<OurStaff />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/book-an-appointment" element={<Appointment />} />
           <Route path="/register" element={<Register />} />
