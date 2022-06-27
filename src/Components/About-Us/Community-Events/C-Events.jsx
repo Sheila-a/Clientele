c2179248b11196d062b0e1f82dd461934542e4dd
Sheila-a <ugwuodo20@gmail.com>
2022-06-27 12:47:27 +0100

    Completed About Us

---
+++ src/Components/About-Us/Community-Events/C-Events.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Navbar from "../../Home/Navbar/Navbar";
+import Footer from "../../Home/Footer/footer";
+import "./C-Events.css";
+
+const CEvents = () => {
+  return (
+    <div>
+      <Navbar />
+      <div className="CEvents">
+        <div className="CEvents-1">dffsd</div>
+      </div>
+      <Footer />
+    </div>
+  );
+};
+
+export default CEvents;
