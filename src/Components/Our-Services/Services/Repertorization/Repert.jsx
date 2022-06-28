661e41cb9d85ce306915bf65de8fe70afa723949
Sheila-a <ugwuodo20@gmail.com>
2022-06-28 16:44:13 +0100

    Updated Repertorization

--- src/Components/Our-Services/Services/Repertorization/Repert.jsx
+++ src/Components/Our-Services/Services/Repertorization/Repert.jsx
@@ -1,13 +1,19 @@
 import React from "react";
 import Navbar from "../../../Home/Navbar/Navbar";
-// import Footer from "../../../Home/Footer/footer";
+import Footer from "../../../Home/Footer/footer";
 import "./Repert.css";
+import Ache_1 from "../../../../Images/Stomachache.gif";
+import Ache_2 from "../../../../Images/Parkinson.gif";
+import Ache_3 from "../../../../Images/Headache.gif";
+import Ache_4 from "../../../../Images/Stress.gif";
+import Rem_1 from "../../../../Images/Thermometer.gif";
+import Rem_2 from "../../../../Images/Exercise.gif";
 
 const Repertory = () => {
   return (
     <div>
       <Navbar />
-      {/* <div className="Rep">
+      <div className="Rep">
         <div className="Rep-Mega-Title">
           <h1>REPERTORIZATION</h1>
           <h2>DETECTING DISEASE & THEIR REMEDIES</h2>
@@ -59,8 +65,140 @@ const Repertory = () => {
             itaque maxime odit eum.
           </p>
         </div>
-      </div> */}
-      {/* <Footer /> */}
+        <div className="Rep-Mega-Title">
+          <h2>DETECTING DISEASES</h2>
+        </div>{" "}
+        <h3 className="Rep-Mega-Title">DISEASE 1</h3>
+        <div className=" Rep-Text-2">
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempora
+            dignissimos culpa, soluta quisquam illum eos? Doloremque, veritatis
+            error cumque enim itaque nihil eos officiis voluptatem laudantium
+            inventore praesentium. Veritatis ad esse sequi minus atque fugiat
+            molestiae dignissimos quos, sunt cum labore veniam quidem distinctio
+            dolorum impedit saepe. Optio tenetur quisquam eius aperiam at vel
+            reiciendis, nam sequi fuga hic praesentium distinctio perferendis,
+            expedita sed officia voluptates nobis ut provident id rem tempore
+            fugiat? Voluptatem dignissimos dolore atque blanditiis, assumenda
+            sequi aspernatur libero aliquam laborum mollitia autem vel nobis
+            ipsam beatae sint in numquam sit reiciendis repellendus eos fugit
+            voluptas incidunt!
+          </p>
+          <img src={Ache_1} alt="" />
+        </div>
+        <h3 className="Rep-Mega-Title">DISEASE 2</h3>
+        <div className=" Rep-Text-2">
+          <img src={Ache_2} alt="" />
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempora
+            dignissimos culpa, soluta quisquam illum eos? Doloremque, veritatis
+            error cumque enim itaque nihil eos officiis voluptatem laudantium
+            inventore praesentium. Veritatis ad esse sequi minus atque fugiat
+            molestiae dignissimos quos, sunt cum labore veniam quidem distinctio
+            dolorum impedit saepe. Optio tenetur quisquam eius aperiam at vel
+            reiciendis, nam sequi fuga hic praesentium distinctio perferendis,
+            expedita sed officia voluptates nobis ut provident id rem tempore
+            fugiat? Voluptatem dignissimos dolore atque blanditiis, assumenda
+            sequi aspernatur libero aliquam laborum mollitia autem vel nobis
+            ipsam beatae sint in numquam sit reiciendis repellendus eos fugit
+            voluptas incidunt!
+          </p>
+        </div>
+        <h3 className="Rep-Mega-Title">DISEASE 3</h3>
+        <div className=" Rep-Text-2">
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempora
+            dignissimos culpa, soluta quisquam illum eos? Doloremque, veritatis
+            error cumque enim itaque nihil eos officiis voluptatem laudantium
+            inventore praesentium. Veritatis ad esse sequi minus atque fugiat
+            molestiae dignissimos quos, sunt cum labore veniam quidem distinctio
+            dolorum impedit saepe. Optio tenetur quisquam eius aperiam at vel
+            reiciendis, nam sequi fuga hic praesentium distinctio perferendis,
+            expedita sed officia voluptates nobis ut provident id rem tempore
+            fugiat? Voluptatem dignissimos dolore atque blanditiis, assumenda
+            sequi aspernatur libero aliquam laborum mollitia autem vel nobis
+            ipsam beatae sint in numquam sit reiciendis repellendus eos fugit
+            voluptas incidunt!
+          </p>
+          <img src={Ache_3} alt="" />
+        </div>
+        <h3 className="Rep-Mega-Title">DISEASE 4</h3>
+        <div className=" Rep-Text-2">
+          <img src={Ache_4} alt="" />
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempora
+            dignissimos culpa, soluta quisquam illum eos? Doloremque, veritatis
+            error cumque enim itaque nihil eos officiis voluptatem laudantium
+            inventore praesentium. Veritatis ad esse sequi minus atque fugiat
+            molestiae dignissimos quos, sunt cum labore veniam quidem distinctio
+            dolorum impedit saepe. Optio tenetur quisquam eius aperiam at vel
+            reiciendis, nam sequi fuga hic praesentium distinctio perferendis,
+            expedita sed officia voluptates nobis ut provident id rem tempore
+            fugiat? Voluptatem dignissimos dolore atque blanditiis, assumenda
+            sequi aspernatur libero aliquam laborum mollitia autem vel nobis
+            ipsam beatae sint in numquam sit reiciendis repellendus eos fugit
+            voluptas incidunt!
+          </p>
+        </div>
+        <h3 className="Rep-Mega-Title">DISEASE 5</h3>
+        <div className=" Rep-Text-2">
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempora
+            dignissimos culpa, soluta quisquam illum eos? Doloremque, veritatis
+            error cumque enim itaque nihil eos officiis voluptatem laudantium
+            inventore praesentium. Veritatis ad esse sequi minus atque fugiat
+            molestiae dignissimos quos, sunt cum labore veniam quidem distinctio
+            dolorum impedit saepe. Optio tenetur quisquam eius aperiam at vel
+            reiciendis, nam sequi fuga hic praesentium distinctio perferendis,
+            expedita sed officia voluptates nobis ut provident id rem tempore
+            fugiat? Voluptatem dignissimos dolore atque blanditiis, assumenda
+            sequi aspernatur libero aliquam laborum mollitia autem vel nobis
+            ipsam beatae sint in numquam sit reiciendis repellendus eos fugit
+            voluptas incidunt!
+          </p>
+          <img src={Ache_1} alt="" />
+        </div>
+        <div className="Rep-Mega-Title">
+          <h2>REMEDIES</h2>
+        </div>
+        <h3 className="Rep-Mega-Title">REMEDY 1</h3>
+        <div className=" Rep-Text-2">
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempora
+            dignissimos culpa, soluta quisquam illum eos? Doloremque, veritatis
+            error cumque enim itaque nihil eos officiis voluptatem laudantium
+            inventore praesentium. Veritatis ad esse sequi minus atque fugiat
+            molestiae dignissimos quos, sunt cum labore veniam quidem distinctio
+            dolorum impedit saepe. Optio tenetur quisquam eius aperiam at vel
+            reiciendis, nam sequi fuga hic praesentium distinctio perferendis,
+            expedita sed officia voluptates nobis ut provident id rem tempore
+            fugiat? Voluptatem dignissimos dolore atque blanditiis, assumenda
+            sequi aspernatur libero aliquam laborum mollitia autem vel nobis
+            ipsam beatae sint in numquam sit reiciendis repellendus eos fugit
+            voluptas incidunt!
+          </p>
+          <img src={Rem_1} alt="" />
+        </div>
+        <h3 className="Rep-Mega-Title">REMEDY 2</h3>
+        <div className=" Rep-Text-2">
+          <img src={Rem_2} alt="" />
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempora
+            dignissimos culpa, soluta quisquam illum eos? Doloremque, veritatis
+            error cumque enim itaque nihil eos officiis voluptatem laudantium
+            inventore praesentium. Veritatis ad esse sequi minus atque fugiat
+            molestiae dignissimos quos, sunt cum labore veniam quidem distinctio
+            dolorum impedit saepe. Optio tenetur quisquam eius aperiam at vel
+            reiciendis, nam sequi fuga hic praesentium distinctio perferendis,
+            expedita sed officia voluptates nobis ut provident id rem tempore
+            fugiat? Voluptatem dignissimos dolore atque blanditiis, assumenda
+            sequi aspernatur libero aliquam laborum mollitia autem vel nobis
+            ipsam beatae sint in numquam sit reiciendis repellendus eos fugit
+            voluptas incidunt!
+          </p>
+        </div>
+      </div>
+      <Footer className="Rep-Foot" />
     </div>
   );
 };
